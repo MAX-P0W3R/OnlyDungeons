5a20c3bafc41bdb18b0210990a52a714abf6531e
maximous <lifeontherun26.2@gmail.com>
2025-12-18 10:21:00 -0700

    Clean up project structure and remove dupes

--- dev/update.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-cd /home/pi/adventure-game
-git pull origin main
-source venv/bin/activate
-pip install -r requirements.txt
-sudo systemctl restart adventure-game
-echo "Game updated and restarted!"
